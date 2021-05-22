5943dc595e6da9e5ea37b768db4e5e2a1cd37375
bhukyarahulnayak <bhukyarahul.nayak.cse19@itbhu.ac.in>
2021-05-22 12:28:41 +0530

    done

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8001/'
+    baseURL: "https://tinderbacend.herokuapp.com"
 })
 
 export default instance;
